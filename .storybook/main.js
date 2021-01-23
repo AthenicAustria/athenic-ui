75ca9422ed70b57000b3ee4173c3a44854596b1e
Behemoth <simonostini@gmail.com>
2021-01-23 20:31:10 +0100

    🚀 LETS FUCKING GOOO STORYBOOK FINALLY WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!! HOLY SHIT

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,14 @@
 const path = require("path");
 
-const custom = require("../webpack.config.js");
+const custom = require("../webpack.config")[1];
 
 module.exports = {
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: { ...config.module, rules: custom.module.rules },
+    };
+  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/preset-scss",
-    "@storybook/addon-essentials",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
