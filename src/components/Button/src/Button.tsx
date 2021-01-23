175d11a303efc57ad8a5169436bb50b991ac57ca
Behemoth <simonostini@gmail.com>
2021-01-23 12:01:22 +0100

    🎨 added button event handler

--- src/components/Button/src/Button.tsx
+++ src/components/Button/src/Button.tsx
@@ -8,13 +8,25 @@ export interface ButtonProps {
   className?: string;
   size?: ButtonSize;
   children?: ReactChildren;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  onHover?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button = ({ text, style, className, size, children }: ButtonProps) => {
+const Button = ({
+  text,
+  style,
+  className,
+  size,
+  children,
+  onClick,
+  onHover,
+}: ButtonProps) => {
   return (
     <button
       style={style ? style : {}}
       className={`button ${size ? size : ""} ${className ? className : ""}`}
+      onClick={onClick ? () => onClick : null}
+      onMouseOver={onHover ? () => onHover : null}
     >
       {text ? text : children ? children : ""}
     </button>
