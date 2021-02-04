1aaccb2a016c8384ee43fa0234bc2f0524b4d8b9
Behemoth <simonostini@gmail.com>
2021-02-04 11:20:59 +0100

    💄 implemented comment types

--- src/components/Comment/src/Comment.tsx
+++ src/components/Comment/src/Comment.tsx
@@ -2,10 +2,11 @@ import { faThumbsDown, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { CSSProperties } from "react";
 import { Avatar } from "../../Avatar";
+import { CommentAuthor } from "../types/CommentTypes";
 import "./Comment.scss";
 
 export interface CommentProps {
-  author: string;
+  author: CommentAuthor;
   text: string;
   likes?: number;
   dislikes?: number;
@@ -30,9 +31,13 @@ const Comment = ({
       className={`comment ${className ? className : ""}`}
       style={style ? style : null}
     >
-      <Avatar size="medium" />
+      <Avatar
+        size="medium"
+        image={author.avatar ? author.avatar : null}
+        link={author.link ? author.link : null}
+      />
       <div className="comment__wrapper">
-        <div className="comment__wrapper__author">{author}</div>
+        <div className="comment__wrapper__author">{author.name}</div>
         <div className="comment__wrapper__text">{text}</div>
         <div className="comment__wrapper__actions">
           {/* <small className="comment__wrapper__actions__like">👍</small>
