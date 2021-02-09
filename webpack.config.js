20933dd4baab7e98f4eb3cfe80f421d1dbf7b5f7
Behemoth <simonostini@gmail.com>
2021-02-09 13:55:44 +0100

    🚀 update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const DtsPlugin = require("dts-webpack-plugin");
 
 module.exports = [
   {
